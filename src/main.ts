b0471c01bb7bad02103ec1ccfe59c72a0560c931
devashish <mshubh612@gmail.com>
2026-02-15 03:01:40 +0530

    added cors for the fronted url prod

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://your-frontend.vercel.app',
+      'https://radiusfrontend.vercel.app/',
     ],
     credentials: true,
   });
